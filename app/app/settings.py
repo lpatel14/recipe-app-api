f3b7b0f9d37fcb055681ba2280c62c73dcb33bcc
Love Patel <lpatel6789@gmail.com>
2022-06-28 13:13:34 -0400

    Flake8 exclusion typo

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$8)3v3d4ajg-v*-
-18y_+o!_6hwnzf%5$85k=hr!k-sonj*f&q7'
+SECRET_KEY = 'django-insecure-$8)3v3d4ajg-v*-18y_+o!_6hwnzf%5$85k=hr!k-sonj*f&q7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,8 +86,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttribute
-        SimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
